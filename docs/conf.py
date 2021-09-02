7d5b195d577eaf3ee7fe11a7256fc6ef2d866101
Mikkel Bue Lykkegaard <m.lykkegaard@exeter.ac.uk>
2021-09-02 15:51:57 +0100

    Bumped version in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ author = 'Mikkel Bue Lykkegaard'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.9.2'
+release = '0.9.3'
 
 
 # -- General configuration ---------------------------------------------------
