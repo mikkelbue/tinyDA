Fixed bug with covariance matrix extraction for newer versions of scipy

tinyDA/proposal.py
@@ -313,7 +313,10 @@ class CrankNicolson(GaussianRandomWalk):
 
     def setup_proposal(self, **kwargs):
         # set the covariance operator
-        self.C = kwargs["posterior"].prior.cov
+        try:
+            self.C = kwargs["posterior"].prior.cov
+        except AttributeError:
+            self.C = kwargs["posterior"].prior.cov_object.covariance
 
         # extract the dimensionality.
         self.d = self.C.shape[0]
@@ -747,10 +750,15 @@ class SingleDreamZ(GaussianRandomWalk):
             except AttributeError:
                 # if the prior is a multivariate_normal, it will not have a .ppf-method.
                 if isinstance(prior, stats._multivariate.multivariate_normal_frozen):
+                    try:
+                        var = np.diag(dist.cov)
+                    except AttributeError:
+                        var = np.diag(dist.cov_object.covariance)
+
                     # instead draw samples from independent normals, according to the prior means and variances.
                     for i in range(self.d):
                         self.Z[:, i] = stats.norm(
-                            loc=prior.mean[i], scale=prior.cov[i, i]
+                            loc=prior.mean[i], scale=np.sqrt(var[i])
                         ).ppf(self.Z[:, i])
                     return
 

tinyDA/utils.py
@@ -261,7 +261,11 @@ def get_ML(posterior, initial_parameters=None, **kwargs):
 
 def grad_log_p(x, dist):
     if isinstance(dist, stats._multivariate.multivariate_normal_frozen):
-        return np.dot(np.linalg.inv(dist.cov), (dist.mean - x))
+        try:
+            cov_inverse = np.linalg.inv(dist.cov)
+        except AttributeError:
+            cov_inverse = np.linalg.inv(dist.cov_object.covariance)
+        return np.dot(cov_inverse, (dist.mean - x))
     else:
         return approx_fprime(x, lambda x: dist.logpdf)
 