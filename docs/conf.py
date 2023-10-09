e9d1d07cd45ffaaf739cafe1e239f927faa6efcf
Mikkel Lykkegaard <mikkel@digilab.co.uk>
2023-10-09 11:50:07 +0100

    Bumped the version

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ author = 'Mikkel Bue Lykkegaard'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.9.12'
+release = '0.9.13'
 
 
 # -- General configuration ---------------------------------------------------
